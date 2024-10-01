878ba34408f0fd68448de0086a36dcce0b46be2b
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-10-01 10:40:29 +0200

    Ejercicio resuelto con sobreescritura de fichero

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -2,18 +2,31 @@ package com.campusdual.classroom;
 
 import com.campusdual.util.Utils;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
 
+        BufferedReader reader;
+        String line;
+        try{
+            reader = new BufferedReader(new FileReader("src/main/resources/data.txt"));
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String stringToSave = generateStringToSave(line);
+        printToFile(stringToSave);
     }
 
     public static String generateStringToSave(String string) {
-
+        return string == null ? generateUserInputToSave() : string;
     }
 
     private static String generateUserInputToSave(){
@@ -27,7 +40,11 @@ public class Exercise32 {
     }
 
     public static void printToFile(String string) {
-
+        try (PrintWriter out = new PrintWriter("src/main/resources/data.txt")) {
+            out.println(string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
